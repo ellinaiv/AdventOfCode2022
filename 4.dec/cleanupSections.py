60f6e278e8cd43bf9fef8603cb3c15361dcd176e
ellinaiv <ellinaivleva@Ellinas-MacBook-Pro.local>
2022-12-04 10:29:20 +0100

    4.dec - part 2

--- 4.dec/cleanupSections.py
+++ 4.dec/cleanupSections.py
@@ -1,14 +1,10 @@
 
-
-
-
-
-def findFullOverlap(filename):
+def findOverlap(filename):
 
     pairs = open(filename, "r")
     fullOverlap = 0
-    for pair in pairs:
 
+    for pair in pairs:
         pair = pair.strip().split(",")
         firstRange = pair[0].split("-")
         secondRange = pair[1].split("-")
@@ -16,11 +12,11 @@ def findFullOverlap(filename):
         firstSet = set([i for i in range(int(firstRange[0]),int(firstRange[1]) + 1)])
         secondSet = set([i for i in range(int(secondRange[0]), int(secondRange[1]) + 1)])
 
-        if firstSet <= secondSet or secondSet <= firstSet:
+        if firstSet & secondSet:
             fullOverlap += 1
 
     print(fullOverlap)
 
 
-findFullOverlap("inputBasic.txt")
-findFullOverlap("input.txt")
+findOverlap("inputBasic.txt")
+findOverlap("input.txt")
