0dadf19aa02203f6900644e6846a1bb346f28113
ellinaiv <ellinaivleva@Ellinas-MacBook-Pro.local>
2022-12-04 10:34:36 +0100

    4.dec - updated if to oneliner

--- 4.dec/cleanupSections.py
+++ 4.dec/cleanupSections.py
@@ -2,7 +2,7 @@
 def findOverlap(filename):
 
     pairs = open(filename, "r")
-    fullOverlap = 0
+    fullOverlapCounter = 0
 
     for pair in pairs:
         pair = pair.strip().split(",")
@@ -12,10 +12,9 @@ def findOverlap(filename):
         firstSet = set([i for i in range(int(firstRange[0]),int(firstRange[1]) + 1)])
         secondSet = set([i for i in range(int(secondRange[0]), int(secondRange[1]) + 1)])
 
-        if firstSet & secondSet:
-            fullOverlap += 1
+        fullOverlapCounter += 1 if firstSet & secondSet else 0
 
-    print(fullOverlap)
+    print(fullOverlapCounter)
 
 
 findOverlap("inputBasic.txt")
